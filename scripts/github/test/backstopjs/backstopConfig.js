7b05bb281dbbf410e2f74911aecb6353fe2b8716
Carl Alberto <carl.alberto@getpantheon.com>
2020-03-30 17:19:48 +0800

    Initial build to Pantheon

---
+++ scripts/github/test/backstopjs/backstopConfig.js
@@ -0,0 +1,64 @@
+// Stash multidev URL, removing any trailing slash
+// const multidevURL = process.env.TEST_SITE_URL.replace(/\/$/, "");
+const multidevURL = process.env.TEST_SITE_URL.replace(/\/$/, "");
+
+// Stash dev URL, removing any trailing slash
+// const devURL = process.env.REFERENCE_SITE_URL.replace(/\/$/, "");
+const devURL = process.env.REFERENCE_SITE_URL.replace(/\/$/, "");
+
+const pathsToTest = {
+    'Homepage': '/',
+    'Blog': '/blog/',
+    'Shop': '/shop/',
+}
+
+let scenariosToTest = [];
+
+for (let [key, value] of Object.entries(pathsToTest)) {
+    scenariosToTest.push({
+        label: key,
+        url: multidevURL + value,
+        referenceUrl: devURL + value,
+        hideSelectors: [],
+        removeSelectors: [],
+        selectorExpansion: true,
+        selectors: [
+            'document',
+        ],
+        readyEvent: null,
+        delay: 1500,
+        misMatchThreshold: 0.1
+    })
+}
+
+module.exports = {
+    id: 'test',
+    viewports: [{
+            name: 'phone',
+            width: 320,
+            height: 480
+        },
+        {
+            name: 'tablet',
+            width: 1024,
+            height: 768
+        }
+    ],
+    scenarios: scenariosToTest,
+    paths: {
+        bitmaps_reference: 'backstop_data/bitmaps_reference',
+        bitmaps_test: 'backstop_data/bitmaps_test',
+        html_report: 'backstop_data/html_report',
+        ci_report: 'backstop_data/ci_report'
+    },
+    report: ['browser', 'CI', 'json'],
+    debug: false,
+    engine: 'puppeteer',
+    engineOptions: {
+        args: ['--no-sandbox']
+    },
+    asyncCaptureLimit: 5,
+    asyncCompareLimit: 50,
+    debug: false,
+    debugWindow: false
+};
