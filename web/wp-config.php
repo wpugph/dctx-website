94b31c3eb96dc6c67ea392e19f56b64a3a22d979
Waren Gonzaga <warengonzaga.dev@gmail.com>
2020-03-31 17:21:02 +0800

    Revise Fix for #2

--- web/wp-config.php
+++ web/wp-config.php
@@ -85,14 +85,14 @@ if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 	 *
 	 * @since 2.6.0
 	 */
-	define( 'AUTH_KEY', '_blank' );
-	define( 'SECURE_AUTH_KEY', '_blank' );
-	define( 'LOGGED_IN_KEY', '_blank' );
-	define( 'NONCE_KEY', '_blank' );
-	define( 'AUTH_SALT', '_blank' );
-	define( 'SECURE_AUTH_SALT', '_blank' );
-	define( 'LOGGED_IN_SALT', '_blank' );
-	define( 'NONCE_SALT', '_blank' );
+	define( 'AUTH_KEY', $_ENV['AUTH_KEY'] );
+    define( 'SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY'] );
+    define( 'LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY'] );
+    define( 'NONCE_KEY', $_ENV['NONCE_KEY'] );
+    define( 'AUTH_SALT', $_ENV['AUTH_SALT'] );
+    define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
+    define( 'LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT'] );
+    define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
 
 endif;
 
