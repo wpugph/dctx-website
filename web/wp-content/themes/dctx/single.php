8206a9cb02f76994766a0aa12bd01f04cf20a949
Kenny Almendral <kennyalmendral@gmail.com>
2020-04-01 09:30:41 +0800

    Update single.php

--- web/wp-content/themes/dctx/single.php
+++ web/wp-content/themes/dctx/single.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
 // phpcs:disable
 
 get_header();
