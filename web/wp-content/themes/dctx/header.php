2e61a4d5016aa6d4dc9f4c01f2920721f3f37fbd
EL Abquina <eabquina@gmail.com>
2020-04-04 21:48:40 +0800

    Update header.php

--- web/wp-content/themes/dctx/header.php
+++ web/wp-content/themes/dctx/header.php
@@ -16,8 +16,6 @@
 <body <?php body_class(); ?>>
 	<!--[if lt IE 8]><p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
 
-	<div id="preloader"></div>
-
 	<header>
 		<div class="header-container">
 			<div class="container clearfix">
