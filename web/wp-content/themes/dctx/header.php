3f5962736739cdb0fab350978f75313359b5e71b
EL Abquina <eladio@pantheon.io>
2020-04-05 15:07:32 +0800

    Google Analytics Snippet on header.php

--- web/wp-content/themes/dctx/header.php
+++ web/wp-content/themes/dctx/header.php
@@ -11,6 +11,16 @@
 	<link rel="icon" type="image/png" href="<?php echo ROOT; ?>favicon.png">
 
 	<?php wp_head(); ?>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162125973-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-162125973-1');
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
